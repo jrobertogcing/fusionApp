ef11e30c5bb9fb5cef43014137316f333c31c2b1
J. Roberto <jose.gonzalez@gonet.us>
2019-03-02 13:39:38 -0600

    Primer Commit

---
+++ fusionApp/Home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  fusionApp
+//
+//  Created by Jose González on 3/2/19.
+//  Copyright © 2019 reset. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ fusionApp/LogIn/LogInViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LogInViewController.swift
+//  fusionApp
+//
+//  Created by Jose González on 3/2/19.
+//  Copyright © 2019 reset. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
